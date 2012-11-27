dd2bf0378ba8a39374c03c0d5b472e1bbbf81ad8
fluppy288@gmail.com <fluppy288@gmail.com@561e42df-ec9d-0dfd-dbcb-2a8051e7129e>
2012-11-27 16:29:46 +0000

    First Upload

---
+++ engine/server/CSetting.lua
@@ -0,0 +1,70 @@
+--[[
+	##########################################################################
+	##                                                                      ##
+	## Project: 'MTA Alan Wake' - Gamemode for Multi Theft Auto San Andreas ##
+	##                      Developer: Noneatme                             ##
+	##           License: See LICENSE in the top level directory            ##
+	##                                                                      ##
+	##########################################################################
+]]
+-- VARIABLES --
+
+serverSetting = {}
+sData = {}
+
+serverSetting["name"] = getServerName()
+serverSetting["maxplayers"] = getMaxPlayers()
+serverSetting["port"] = getServerPort()
+
+setFPSLimit(60)
+setWeaponProperty("deagle", "pro", "damage", 150)
+setWeaponProperty("deagle", "pro", "accuracy", 1000)
+setSkyGradient(20, 20, 20, 0, 0, 0)
+
+function getServerSetting(name)
+	return serverSettig[name];
+end
+
+startResource(getResourceFromName("bone_attach"))
+
+
+-- FUNcTIONS --
+
+
+local fadeP = {}
+function setInPosition(thePlayer, x, y, z, interior, typ, dim)
+	if not(thePlayer) then return end
+	if (getElementType(thePlayer) == "vehicle") then return end
+	if(isPedInVehicle(thePlayer)) then return end
+	if not(x) or not(y) or not(z) then return end
+	if not(interior) then interior = 0 end
+	if(fadeP[thePlayer] == 1) then return end
+	fadeP[thePlayer] = 1
+	fadeCamera(thePlayer, false)
+	setElementFrozen(thePlayer, true)
+	setTimer(
+		function()
+		fadeP[thePlayer] = 0
+		setElementPosition(thePlayer, x, y, z)
+		setElementInterior(thePlayer, interior)
+		if(dim) then setElementDimension(thePlayer, dim) end
+		fadeCamera(thePlayer, true)
+		if not(typ) then
+			setElementFrozen(thePlayer, false)
+		else
+			if(typ == true)  then
+				setTimer(setElementFrozen, 1000, 1, thePlayer, false)
+			end
+		end
+	end, 1000, 1)
+end
+
+function callServerFunction(funcname, ...)
+    local arg = { ... }
+    if (arg[1]) then
+        for key, value in next, arg do arg[key] = tonumber(value) or value end
+    end
+    loadstring("return "..funcname)()(unpack(arg))
+end
+addEvent("onClientCallsServerFunction", true)
+addEventHandler("onClientCallsServerFunction", resourceRoot , callServerFunction)
