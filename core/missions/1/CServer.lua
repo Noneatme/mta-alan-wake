5b3fa248e6f76c179122aac5b1d182b5690d7a4d
fluppy288@gmail.com <fluppy288@gmail.com@561e42df-ec9d-0dfd-dbcb-2a8051e7129e>
2012-11-27 16:20:14 +0000

    First Upload

---
+++ core/missions/1/CServer.lua
@@ -0,0 +1,9 @@
+--[[
+	##########################################################################
+	##                                                                      ##
+	## Project: 'MTA Alan Wake' - Gamemode for Multi Theft Auto San Andreas ##
+	##                      Developer: Noneatme                             ##
+	##           License: See LICENSE in the top level directory            ##
+	##                                                                      ##
+	##########################################################################
+]]
