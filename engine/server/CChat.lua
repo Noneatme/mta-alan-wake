fb96443daaf136a2279b4d57cc6c04a7989f16c5
fluppy288@gmail.com <fluppy288@gmail.com@561e42df-ec9d-0dfd-dbcb-2a8051e7129e>
2012-11-27 16:28:39 +0000

    First Upload

---
+++ engine/server/CChat.lua
@@ -0,0 +1,21 @@
+--[[
+	##########################################################################
+	##                                                                      ##
+	## Project: 'MTA Alan Wake' - Gamemode for Multi Theft Auto San Andreas ##
+	##                      Developer: Noneatme                             ##
+	##           License: See LICENSE in the top level directory            ##
+	##                                                                      ##
+	##########################################################################
+]]
+
+
+local sFunc = {}
+local sSetting = {}
+
+
+-- FUNCTIONS --
+
+
+
+
+-- EVENT HANDLERS --
