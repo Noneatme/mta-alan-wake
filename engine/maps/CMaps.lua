9eb1b23e94aff617831ea010746d86babe20961f
fluppy288@gmail.com <fluppy288@gmail.com@561e42df-ec9d-0dfd-dbcb-2a8051e7129e>
2012-11-27 16:30:33 +0000

    First Upload

---
+++ engine/maps/CMaps.lua
@@ -0,0 +1,14 @@
+--[[
+	##########################################################################
+	##                                                                      ##
+	## Project: 'MTA Alan Wake' - Gamemode for Multi Theft Auto San Andreas ##
+	##                      Developer: Noneatme                             ##
+	##           License: See LICENSE in the top level directory            ##
+	##                                                                      ##
+	##########################################################################
+]]
+
+-- REMOVED OBJECTS --
+
+removeWorldModel(672, 50, 65.7890625, -168.7265625, 0.21875) -- BAR BAUM 
+removeWorldModel(0, 50, 65.7890625, -168.7265625, 0.21875) -- LOD 
