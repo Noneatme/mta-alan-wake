8388369f4b34b6edf20fb9119abca5a10c222e13
fluppy288@gmail.com <fluppy288@gmail.com@561e42df-ec9d-0dfd-dbcb-2a8051e7129e>
2012-11-27 16:14:59 +0000

    Edit

--- core/ai/CZombie.lua
+++ core/ai/CZombie.lua
@@ -2,8 +2,8 @@
 	##########################################################################
 	##                                                                      ##
 	## Project: 'MTA Alan Wake' - Gamemode for Multi Theft Auto San Andreas ##
-	##                      Developer: Noneatme								##
-	##           License: See LICENSE in the top level directory	        ##
+	##                      Developer: Noneatme                             ##
+	##           License: See LICENSE in the top level directory            ##
 	##                                                                      ##
 	##########################################################################
 ]]
